0b50c7d99b7a7dba7cf62fb7c5ad7ecf4846201b
Jhops <jspadilla017@gmail.com>
2021-12-27 16:15:42 -0500

    second comand

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,12 +1,12 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Hola';
+const nuevaString = 'Hola mundo';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
 
-// Crea una variable booleana:
+// Crea una variable booleana:ñ
 const nuevoBool = null;
 
 // Resuelve el siguiente problema matemático:
